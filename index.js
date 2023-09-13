fa31a7e59f96cb479874bd0c8f2a2ed952b199ef
Starszhangtao <3054296577@qq.com>
2023-09-13 10:35:58 +0800

    mate60pro

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-  // 选择将观察突变的节点
-  var targetNode = document.querySelector('.product-button02');
-  // 观察者的选项(要观察哪些突变)
-  var config = { attributes: true, childList: true, subtree: true };
+// ==UserScript==
+// @name         抢mate60pro
+// @version      1.0
+// @description  try to take over the world!
+// @author       Stars
+// @match        https://www.vmall.com/product/*
+// @icon         https://res.vmallres.com/pimages//uomcdn/CN/pms/202309/gbom/6942103109560/78_78_085E54DDF8265F682ACEA3C84D434E93mp.png
+// @grant        none
+// ==/UserScript==
 
-  // 当观察到突变时执行的回调函数
-  var callback = function(mutationsList) {
-      mutationsList.forEach(function(item,index){
-          if (item.type == 'childList') {
-              console.log('有节点发生改变，当前节点的内容是:'+ item.target.innerHTML);
-          } else if (item.type == 'attributes') {
-                 console.log("我有在执行吗")
-              //  console.log('修改了'+item.attributeName+'属性');
-              handlerthing()
-          }
-      });
-  };
+(function() {
+    'use strict';
+    // 选择将观察突变的节点
+    var targetNode = document.querySelector('.product-button02');
+    // 观察者的选项(要观察哪些突变)
+    var config = { attributes: true, childList: true, subtree: true };
 
-  // 创建一个链接到回调函数的观察者实例
-  var observer = new MutationObserver(callback);
+    // 当观察到突变时执行的回调函数
+    var callback = function(mutationsList) {
+        mutationsList.forEach(function(item,index){
+            if (item.type == 'childList') {
+                console.log('有节点发生改变，当前节点的内容是:'+ item.target.innerHTML);
+            } else if (item.type == 'attributes') {
+                   console.log("我有在执行吗")
+                //  console.log('修改了'+item.attributeName+'属性');
+                handlerthing()
+            }
+        });
+    };
 
-  // 开始观察已配置突变的目标节点
-  observer.observe(targetNode, config);
-  //处理抢手机
-  function handlerthing(){
-      targetNode.click()
-  }
+    // 创建一个链接到回调函数的观察者实例
+    var observer = new MutationObserver(callback);
+
+    // 开始观察已配置突变的目标节点
+    observer.observe(targetNode, config);
+    //处理抢手机
+    function handlerthing(){
+        targetNode.click()
+    }
+})();
